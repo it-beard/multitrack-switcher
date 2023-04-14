016b330794f2aaf0dd1f16d827c2a9749c94da6d
itbeard <iamitbeard@gmail.com>
2023-04-14 16:30:07 +0200

    add duration of diluter frame setting and build new version

--- PRProjMulticamCreator/PRProjMulticamCreator/MainWindow.axaml.cs
+++ PRProjMulticamCreator/PRProjMulticamCreator/MainWindow.axaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using PRProjMulticamCreator.Core;
@@ -29,6 +30,94 @@ public partial class MainWindow : Window
         SecondSpeakerSensitivity.Text = "0.065";
         DiluteIterations.Value = 3;
         DiluteFrameDuration.Value = 4;
+    }
+
+    private async void StartButton_Click(object sender, RoutedEventArgs e)
+    {
+        ProgressBar.IsVisible = true;
+        var vm = DataContext as MainWindowViewModel;
+
+        // load XML
+        var gzipService = new GzipService();
+        var prproj = gzipService.LoadXml(PrprojPath);
+
+        // save files (for Debugging)
+        // await gzipService.DecompressAndSave(PrprojPath, Constants.DecompressedPath);
+        // prproj.Load(Constants.DecompressedPath);
+
+        // process frames for two Tracks
+        var frameService = new FrameService();
+        var primaryFrames = frameService.GetListOfPureWavFrames(
+            Speaker1Wav,
+            FirstSpeakerSensitivity.Text.ToDouble(),
+            Constants.PremierProPrimaryTrackIndex); // get real frames from primary Track
+        primaryFrames = frameService.RemoveNoiseFrames(primaryFrames);
+        primaryFrames = frameService.MergeThroughSilence(primaryFrames);
+        var secondarySyntheticFrames = frameService.GetSyntheticFrames(primaryFrames); // create synthetic frames for secondary Track
+
+        var allFrames = new List<FrameModel>();
+        allFrames.AddRange(primaryFrames);
+        allFrames.AddRange(secondarySyntheticFrames);
+        allFrames = allFrames.OrderBy(a => a.InPoint).ToList(); // sort frames by InPoint
+
+        // mix short frames from real secondary Track
+        var secondaryFrames = frameService.GetListOfPureWavFrames(
+            Speaker2Wav,
+            SecondSpeakerSensitivity.Text.ToDouble(),
+            Constants.PremierProSecondaryTrackIndex); // get real frames from secondary Track
+        secondaryFrames = frameService.RemoveNoiseFrames(secondaryFrames);
+        secondaryFrames = frameService.MergeThroughSilence(secondaryFrames);
+        var secondaryShortFrames = frameService.RemoveLongFrames(secondaryFrames);
+        var allWithSecondaryShortFrames = frameService.AddShortFramesToAllFrames(secondaryShortFrames, allFrames); //merge short secondary frames with all frames
+
+        // start dilute long frames
+        var result = new List<FrameModel>();
+        if (DiluteIterations.Value > 0)
+        {
+            for (int i = 0; i < DiluteIterations.Value; i++)
+            {
+                result = frameService.DiluteLongFrames(allWithSecondaryShortFrames, vm.DiluteModeValue, DiluteFrameDuration.Value); // dilute long frames
+            }
+        }
+        else
+        {
+            result = allWithSecondaryShortFrames;
+        }
+
+        // update multicam track
+        var nodeService = new NodeService();
+        nodeService.UpdateMulticamTrack(prproj, result);
+
+        Result.Text = "Done!";
+        ProgressBar.IsVisible = false;
+        await ShowSaveFileDialog(prproj);
+
+        // save files (for Debugging)
+        // prproj.Save(Constants.TempFilePath);
+        // gzipService.CompressAndSave(Constants.CompressedFilePath, Constants.TempFilePath);
+    }
+
+    private async Task ShowSaveFileDialog(XmlDocument xmlDoc)
+    {
+        SaveFileDialog saveFileDialog = new SaveFileDialog
+        {
+            Title = "Save .prproj File",
+            DefaultExtension = "prproj",
+            Filters = new List<FileDialogFilter>
+            {
+                new FileDialogFilter { Name = "Premiere Pro Project", Extensions = new List<string> { "prproj" } }
+            }
+        };
+
+        var result = await saveFileDialog.ShowAsync(this); // 'this' refers to your window or control
+
+        if (!string.IsNullOrEmpty(result))
+        {
+            await using FileStream fs = File.Create(result);
+            await using GZipStream gzipStream = new GZipStream(fs, CompressionLevel.Optimal);
+            await using XmlTextWriter xmlWriter = new XmlTextWriter(gzipStream, Encoding.UTF8);
+            xmlDoc.Save(xmlWriter);
+        }
     }
 
     private async void SelectPrprojFileButton_Click(object sender, RoutedEventArgs e)
@@ -78,6 +167,22 @@ public partial class MainWindow : Window
         }
     }
 
+    private void DiluteIterationsUpDown_PropertyChanged(object sender, AvaloniaPropertyChangedEventArgs e)
+    {
+        if (e.Property == NumericUpDown.ValueProperty)
+        {
+            if (e.NewValue != null && (double)e.NewValue > 0)
+            {
+                DiluteFramesDurationPanel.IsEnabled = true;
+                DiluteModeBox.IsEnabled = true;
+            }
+            else
+            {
+                DiluteFramesDurationPanel.IsEnabled = false;
+                DiluteModeBox.IsEnabled = false;
+            }
+        }
+    }
 
     private void CheckStartButtonEnabled()
     {
@@ -88,86 +193,6 @@ public partial class MainWindow : Window
         else
         {
             StartButton.IsEnabled = false;
-        }
-    }
-
-    private async void StartButton_Click(object sender, RoutedEventArgs e)
-    {
-        ProgressBar.IsVisible = true;
-        var vm = DataContext as MainWindowViewModel;
-
-        // load XML
-        var gzipService = new GzipService();
-        var prproj = gzipService.LoadXml(PrprojPath);
-
-        // save files (for Debugging)
-        // await gzipService.DecompressAndSave(PrprojPath, Constants.DecompressedPath);
-        // prproj.Load(Constants.DecompressedPath);
-
-        // process frames for two Tracks
-        var frameService = new FrameService();
-        var primaryFrames = frameService.GetListOfPureWavFrames(
-            Speaker1Wav,
-            FirstSpeakerSensitivity.Text.ToDouble(),
-            Constants.PremierProPrimaryTrackIndex); // get real frames from primary Track
-        primaryFrames = frameService.RemoveNoiseFrames(primaryFrames);
-        primaryFrames = frameService.MergeThroughSilence(primaryFrames);
-        var secondarySyntheticFrames = frameService.GetSyntheticFrames(primaryFrames); // create synthetic frames for secondary Track
-
-        var allFrames = new List<FrameModel>();
-        allFrames.AddRange(primaryFrames);
-        allFrames.AddRange(secondarySyntheticFrames);
-        allFrames = allFrames.OrderBy(a => a.InPoint).ToList(); // sort frames by InPoint
-
-        // mix short frames from real secondary Track
-        var secondaryFrames = frameService.GetListOfPureWavFrames(
-            Speaker2Wav,
-            SecondSpeakerSensitivity.Text.ToDouble(),
-            Constants.PremierProSecondaryTrackIndex); // get real frames from secondary Track
-        secondaryFrames = frameService.RemoveNoiseFrames(secondaryFrames);
-        secondaryFrames = frameService.MergeThroughSilence(secondaryFrames);
-        var secondaryShortFrames = frameService.RemoveLongFrames(secondaryFrames);
-        var allWithSecondaryShortFrames = frameService.AddShortFramesToAllFrames(secondaryShortFrames, allFrames); //merge short secondary frames with all frames
-
-        var result = new List<FrameModel>();
-        for (int i = 0; i < DiluteIterations.Value; i++)
-        {
-            result = frameService.DiluteLongFrames(allWithSecondaryShortFrames, vm.DiluteModeValue, DiluteFrameDuration.Value); // dilute long frames
-        }
-
-        // update multicam track
-        var nodeService = new NodeService();
-        nodeService.UpdateMulticamTrack(prproj, result);
-
-        Result.Text = "Done!";
-        ProgressBar.IsVisible = false;
-        await ShowSaveFileDialog(prproj);
-
-        // save files (for Debugging)
-        // prproj.Save(Constants.TempFilePath);
-        // gzipService.CompressAndSave(Constants.CompressedFilePath, Constants.TempFilePath);
-    }
-
-    private async Task ShowSaveFileDialog(XmlDocument xmlDoc)
-    {
-        SaveFileDialog saveFileDialog = new SaveFileDialog
-        {
-            Title = "Save .prproj File",
-            DefaultExtension = "prproj",
-            Filters = new List<FileDialogFilter>
-            {
-                new FileDialogFilter { Name = "Premiere Pro Project", Extensions = new List<string> { "prproj" } }
-            }
-        };
-
-        var result = await saveFileDialog.ShowAsync(this); // 'this' refers to your window or control
-
-        if (!string.IsNullOrEmpty(result))
-        {
-            await using FileStream fs = File.Create(result);
-            await using GZipStream gzipStream = new GZipStream(fs, CompressionLevel.Optimal);
-            await using XmlTextWriter xmlWriter = new XmlTextWriter(gzipStream, Encoding.UTF8);
-            xmlDoc.Save(xmlWriter);
         }
     }
 }
